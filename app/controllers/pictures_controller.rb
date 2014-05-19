5d6a3519919e5f07fa3f51c3c9d412ed2bd0c177
Mark Laurenz <m-laurenz@kellogg.northwestern.edu>
2014-05-18 21:07:06 -0500

    Part 1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,4 +10,14 @@ class PicturesController < ApplicationController
   def new
   end
 
+  def create
+    Picture.all.each do |pic|
+      p = Picture.new
+      p.source = pic[:source]
+      p.caption = pic[:caption]
+      p.save
+      end
+    redirect_to("http://localhost:3000/all_pictures")
+  end
+
 end

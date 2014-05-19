bfee2d7580adecb387f93a8e9e93ea08e385f3da
Mark Laurenz <m-laurenz@kellogg.northwestern.edu>
2014-05-18 20:41:20 -0500

    Part 1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,13 @@
 class PicturesController < ApplicationController
+
   def show
+    @picture_images = Picture.find_by({ :id => params[:id] })
   end
+
+  def index
+  end
+
+  def new
+  end
+
 end
